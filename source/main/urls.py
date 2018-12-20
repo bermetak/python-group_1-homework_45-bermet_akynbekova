1ed23a547c825bfba25770f545d2f19acbc3dcdf
Farid <thesomeq@gmail.com>
2018-12-20 21:49:21 +0600

    Added example pages to display photo and query for food name in order.

--- source/main/urls.py
+++ source/main/urls.py
@@ -18,8 +18,12 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from webapp.views import FoodDetailView, OrderDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('order/<int:pk>', OrderDetailView.as_view(), name='order_detail'),
+    path('food/<int:pk>', FoodDetailView.as_view(), name='food_detail')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from webapp.models import Food, Order
+from django.views.generic import DetailView
 
-# Create your views here.
+
+class FoodDetailView(DetailView):
+    model = Food
+    template_name = 'food_detail.html'
+
+
+class OrderDetailView(DetailView):
+    model = Order
+    template_name = 'order_detail.html'
