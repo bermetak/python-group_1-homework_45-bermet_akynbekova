91d9c936250929ae4390ba95b73f0d65c6867cc4
Farid <thesomeq@gmail.com>
2018-12-20 22:00:16 +0600

    Added __str__() methods everywhere

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -6,12 +6,18 @@ class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='employee', verbose_name='Пользователь')
     phone = models.CharField(max_length=50, verbose_name='Телефон')
 
+    def __str__(self):
+        return self.user.get_full_name()
+
 
 class Food(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название')
     description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Описание')
     photo = models.ImageField(verbose_name='Фотография')
     price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -36,8 +42,14 @@ class Order(models.Model):
     operator = models.ForeignKey(User, null=True, blank=True, related_name='orders', verbose_name='Оператор', on_delete=models.PROTECT)
     courier = models.ForeignKey(User, null=True, blank=True, related_name='delivered', verbose_name='Курьер', on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.contact_phone
+
 
 class OrderFood(models.Model):
     order = models.ForeignKey(Order, related_name='foods', verbose_name='Заказ', on_delete=models.PROTECT)
     food = models.ForeignKey(Food, related_name='+', verbose_name='Блюдо', on_delete=models.PROTECT)
     amount = models.IntegerField(verbose_name='Количество')
+
+    def __str__(self):
+        return str(self.food)
